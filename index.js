608297dda0725146fde1e1f91f39d573a28c7512
Carlos Martins <carlosouza.ces@gmail.com>
2025-05-29 13:44:43 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,44 +1,40 @@
-<div class="slideshow-container">
+let slideIndex = 1;
+showSlides(slideIndex);
 
- <div class="mySlides fade">
-    <div class="numbertext">1 / 4</div>
-    <img src="https://github.com/DWBA4/dwba4_semana_imagens/blob/master/A.png?raw=true" style="width:100%">
-    <div class="text">Caption Text</div>
-  </div>
 
-  <div class="mySlides fade">
-    <div class="numbertext">2 / 4</div>
-    <img src="https://github.com/DWBA4/dwba4_semana_imagens/blob/master/B.jpg?raw=true" style="width:100%">
-    <div class="text">Caption Two</div>
-  </div>
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
 
-  <div class="mySlides fade">
-    <div class="numbertext">3 / 4</div>
-    <img src="https://github.com/DWBA4/dwba4_semana_imagens/blob/master/C.jpg?raw=true" style="width:100%">
-    <div class="text">Caption Three</div>
-  </div>
 
-    <div class="mySlides fade">
-    <div class="numbertext">4 / 4</div>
-    <h2 style="text-align:center">Conheça os desafios de inovação</h2>
-    <div style="display: flex; justify-content: center;">
-      <iframe width="560" height="315" 
-        src="https://www.youtube.com/embed/lWmz5aS2O2I" 
-        title="Desafio de Inovação" 
-        frameborder="0" 
-        allowfullscreen>
-      </iframe>
-    </div>
-  </div>
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
 
-  <a class="prev" onclick="plusSlides(-1)">&#10094;</a>
-  <a class="next" onclick="plusSlides(1)">&#10095;</a>
-</div>
-<br>
 
-<div style="text-align:center">
-  <span class="dot" onclick="currentSlide(1)"></span>
-  <span class="dot" onclick="currentSlide(2)"></span>
-  <span class="dot" onclick="currentSlide(3)"></span>
-  <span class="dot" onclick="currentSlide(4)"></span>
-</div>
+function showSlides(n) {
+  let i;
+  const slides = document.getElementsByClassName("mySlides");
+  const dots = document.getElementsByClassName("dot");
+
+  if (n > slides.length) {
+    slideIndex = 1;
+  }    
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+
+
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+
+
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+
+
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+}
